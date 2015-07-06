9f1df6613516f76457c49cacd65badb314959d2a
Patrick Bolduc <pbolduc@rheinmetall.ca>
2015-07-05 22:57:31 -0400

    Lesson 1 : Data types

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,58 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"name" : "Patrick Bolduc",
+	"role" : "Software Developer",
+	"contacts" : {
+		"mobile" : "514-717-1165",
+		"email" : "patrick.bolduc@gmail.com",
+		"github" : "https://github.com/pbolduc123",
+		"twitter" : "patb986",
+		"location" : "Montréal, Québec, Canada"
+	},
+	"welcomeMessage" : "Passionate Software Developer",
+	"skills" : ["Java, C++, Android"],
+	"biopic" : "images/fry.jpg"
+} 
+
+var education = {
+	"schools" : [
+	{
+		"name" : "Université de Sherbooke",
+		"location" : "Sherbooke, Québec, Canada",
+		"degree" : "BA",
+		"majors" : "Computer Engineering",
+		"dates" : 2011,
+		"url" : "http://www.usherbrooke.ca"
+	}
+	],
+	"onlineCourses": [
+	{
+		"title": "Javascript Basics",
+		"school" : "Udacity",
+		"date" : 2015,
+		"url" : "http://www.udacity.com"
+	}
+	]
+}
+
+var work = {
+	"jobs" : [
+	{
+		"employer" : "Rheinmetall Canada",
+		"title" : "Java Software Developer",
+		"location" : "Saint-Jean-sur-Richelieu, Québec, Canada",
+		"dates" : "2012-Present",
+		"description" : ""
+	}
+	]
+}
+
+var projects = {
+	"projects" : [
+	{
+		"title" : "Modern Art UI"
+	},
+	{
+		"title" : "Portfolio"
+	}
+	]
+}
