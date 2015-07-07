7f242dc6fc066c60e30f068e381650c86bad60a3
Patrick Bolduc <pbolduc@rheinmetall.ca>
2015-07-06 23:20:11 -0400

    lesson 2 and 3

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,7 +9,7 @@ var bio = {
 		"location" : "Montréal, Québec, Canada"
 	},
 	"welcomeMessage" : "Passionate Software Developer",
-	"skills" : ["Java, C++, Android"],
+	"skills" : ["Java", "C++", "Android"],
 	"biopic" : "images/fry.jpg"
 } 
 
@@ -41,7 +41,7 @@ var work = {
 		"title" : "Java Software Developer",
 		"location" : "Saint-Jean-sur-Richelieu, Québec, Canada",
 		"dates" : "2012-Present",
-		"description" : ""
+		"description" : "Develop Java application for desktops, androids and other embedded devices"
 	}
 	]
 }
@@ -49,10 +49,70 @@ var work = {
 var projects = {
 	"projects" : [
 	{
-		"title" : "Modern Art UI"
+		"title" : "Resume",
+		"dates" : "2015",
+		"description" : "My resume using Javascript",
+		"images" : "images/197x148.gif"
 	},
 	{
-		"title" : "Portfolio"
+		"title" : "Portfolio",
+		"dates" : "2015",
+		"description" : "My Portfolio using HTML and CSS",
+		"images" : "images/197x148.gif"
 	}
 	]
 }
+
+projects.display = function() {
+	for(project in projects.projects)
+	{
+		var p = projects.projects[project];
+		console.log(p);
+		$("#projects").append(HTMLprojectStart); 
+
+		$(".project-entry:last").append(
+			HTMLprojectTitle.replace("%data%", p.title) + 
+			HTMLprojectDates.replace("%data%", p.dates) + 
+			HTMLprojectDescription.replace("%data%", p.description) + 
+			HTMLprojectImage.replace("%data%", p.images));
+	}
+}
+
+if(bio.skills.length > 0)
+{
+	$("#header").append(HTMLskillsStart);
+	$("#skills").append(HTMLskills.replace("%data%", bio.skills[0]));
+	$("#skills").append(HTMLskills.replace("%data%", bio.skills[1]));
+	$("#skills").append(HTMLskills.replace("%data%", bio.skills[2]));
+}
+
+
+displayWork();
+projects.display();
+$("#main").append(internationalizeButton);
+
+function displayWork()
+{
+	for(job in work.jobs)
+	{
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedEmployer + formattedTitle + formattedLocation + formattedDates + formattedDescription);
+	}
+}
+
+function inName(name_obj)
+{
+	var words = name_obj.split(" ");
+	return words[0] + " " + words[1].toUpperCase();
+}
+
+$(document).click(function(loc) {
+	logClicks(loc.pageX, loc.pageY);
+});
+
+$("#map-div").append(googleMap);
